30e794c2779957b1549d4fb03901fbd88b961d94
Jonczyk <krzysztof.jonczyk@goldenline.pl>
2016-08-10 21:03:14 +0200

    Basic application

---
+++ code/src/AppBundle/Controller/NoteController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Note;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class NoteController
+ * @package AppBundle\Controller
+ */
+class NoteController extends Controller
+{
+    /**
+     * @Route("/list", name="note_list")
+     *
+     * @return Response
+     */
+    public function listAction() : Response
+    {
+        $notes = $this->getNotes();
+
+        // replace this example code with whatever you need
+        return $this->render(':note:list.html.twig', ["notes" => $notes]);
+    }
+
+    /**
+     * @return Note[]
+     */
+    private function getNotes() : array
+    {
+        $note1 = new Note();
+        $note1
+            ->setId(1)
+            ->setTitle("first note")
+            ->setContent("first note content")
+            ->setRemindAt(new \DateTime("+1 hour"))
+            ->setCreatedAt(new \DateTime());
+
+        $note2 = new Note();
+        $note2
+            ->setId(2)
+            ->setTitle("second note")
+            ->setContent("second note content")
+            ->setRemindAt(new \DateTime("+1 day"))
+            ->setCreatedAt(new \DateTime());
+
+        $note3 = new Note();
+        $note3
+            ->setId(2)
+            ->setTitle("third note")
+            ->setContent("third note content")
+            ->setRemindAt(new \DateTime("+2 days 12:00:00"))
+            ->setCreatedAt(new \DateTime());
+
+        return [$note1, $note2, $note3];
+    }
+}
---
+++ code/src/AppBundle/Entity/Note.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class Note
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
+     */
+    private $remindAt;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     *
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return $this
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getRemindAt()
+    {
+        return $this->remindAt;
+    }
+
+    /**
+     * @param \DateTime $remindAt
+     *
+     * @return $this
+     */
+    public function setRemindAt($remindAt)
+    {
+        $this->remindAt = $remindAt;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     *
+     * @return $this
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
